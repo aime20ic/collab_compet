a026842b4b5ab9bc6e6f5ca3039e023189723595
Ivan Caceres <aime20ic@gmail.com>
2021-06-09 22:25:47 -0500

    added files from drl project 2

---
+++ collab_compet/agent.py
@@ -0,0 +1,203 @@
+import json
+import time
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+
+from pathlib import Path
+from collections import deque
+
+
+class Agent():
+    """
+    Agent
+    """
+
+    def __init__(self, env, seed=None, verbose=False, **kwargs):
+        """
+        Initialize an Agent object
+
+        Args:
+            env (UnityEnv): Simulation environment
+            seed (int): Seed
+            verbose (bool): Verbosity
+
+        Returns:
+            Agent object
+        
+        """
+
+        # Set agent class variables
+        self.name = kwargs.get('name', 'agent')
+        self.run_id = kwargs.get('run_id', int(time.time()))
+        self.output = kwargs.get('output', 
+            Path('./output/' + str(self.run_id) + '/'))
+        self.rng = None
+        self.rng_seed = None
+        self.verbose = verbose
+
+        # Set environment variables
+        self.env = env
+        
+        # Set agent training hyperparameters
+        self.batch_size = kwargs.get('batch_size', 64)          # minibatch size
+        self.gamma = kwargs.get('gamma', 0.99)                  # discount factor
+        self.lr = kwargs.get('lr', 5e-4)                        # learning rate 
+        self.optimizer = None
+        self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+        # Set seed
+        self.seed(seed)
+
+        return
+
+    def load(self, path):
+        """
+        Load weights specified in path into model
+
+        Args:
+            path (Path): Saved model weights to load
+
+        Returns:
+            None
+        
+        """
+        pass
+    
+    def act(self, state):
+        """
+        Returns actions for given state as per current policy
+        
+        Args:
+            state (array): Current state
+
+        Returns:
+            Actions (array)
+        
+        """
+        pass
+
+    def seed(self, seed=None):
+        """
+        Set seed for random number generation, sampling, & repeatibility
+        
+        Args:
+            seed (int): Seed for random number generation
+
+        Returns:
+            None
+        
+        """
+
+        # Error check
+        if not isinstance(seed, int) and seed is not None:
+            raise ValueError('Specified seed must be integer or None')
+
+        # Set seed & random number generator
+        self.rng_seed = seed
+        self.rng = np.random.RandomState(seed)
+
+        return
+
+    def _log_parameters(self):
+        """
+        Log agent parameters as JSON
+
+        Args:
+            None
+        
+        Returns:
+            None
+        
+        """
+
+        # Create file path
+        path = self.output / (str(self.run_id) + '__' + self.name + '.json')
+        path.parent.mkdir(parents=True, exist_ok=True)
+
+        # Make sure parameters are JSON serializable
+        parameters = vars(self).copy()
+        for key, value in parameters.items():
+            try:
+                json.dumps(value)
+            except TypeError:
+                parameters[key] = str(value)
+        
+        # Save as JSON
+        with open(path, 'w') as file:
+            json.dump(parameters, file, indent=4, sort_keys=True)
+
+        return
+
+    @staticmethod
+    def write2path(text, path):
+        """
+        Write text to path object
+
+        Args:
+            text (str): Text to log
+            path (Path): Path object
+        
+        Returns:
+            None
+        
+        """
+
+        # Create path
+        path.parent.mkdir(parents=True, exist_ok=True)
+
+        # Write text to path
+        if not path.exists():
+            path.write_text(text)
+        else:
+            with path.open('a') as f:
+                f.write(text)
+        
+        return
+
+    def plot_performance(self, scores, name, window_size):
+        """
+        Plot summary of DQN performance on environment
+
+        Args:
+            scores (list of float): Score per simulation episode
+            name (Path): Name for file
+            window_size (int): Windowed average size
+
+        """
+        window_avg = []
+        window_std = []
+        window = deque(maxlen=window_size)
+        
+        # Create avg score
+        avg = [np.mean(scores[:i+1]) for i in range(len(scores))]
+        for i in range(len(scores)):
+            window.append(scores[i])
+            window_avg.append(np.mean(window))
+            window_std.append(np.std(window))
+
+        # Create 95% confidence interval (2*std)
+        lower_95 = np.array(window_avg) - 2 * np.array(window_std)
+        upper_95 = np.array(window_avg) + 2 * np.array(window_std)
+
+        # Plot scores
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        plt.scatter(np.arange(len(scores)), scores, color='cyan', label='Scores')
+        plt.plot(avg, color='blue', label='Average')
+        plt.plot(window_avg, color='red', label='Windowed Average (n={})'.format(window_size))
+        plt.fill_between(np.arange(len(window_std)), lower_95, upper_95, color='red', alpha=0.1)
+        plt.ylabel('Score')
+        plt.xlabel('Episode #')
+        plt.legend(loc='upper left')
+        ax.margins(y=.1, x=.1) # Help with scaling
+        plt.show(block=False)
+
+        # Save figure
+        name.parent.mkdir(parents=True, exist_ok=True)
+        plt.savefig(name)
+        plt.close()
+
+        return
+
+
---
+++ collab_compet/unity_env.py
@@ -0,0 +1,176 @@
+import numpy as np
+
+from unityagents import UnityEnvironment
+
+
+class UnityEnv():
+    """
+    Unity environment simulation
+    """
+
+    def __init__(self, path, name, worker_id=0, train=False, seed=None, verbose=False):
+        """
+        Class constructor / UnityEnv initializer
+
+        Args:
+            path (string): Path to Unity simulation
+            name (string): Name for environment
+            worker_id (int): Indicates port to use for communication
+            train (bool): Unity environment training mode
+            seed (int): Seed for random number generation
+            verbose (bool): Verbosity
+        
+        Returns:
+            UnityEnv class object
+        
+        """
+
+        # Initialize environment variables
+        self.env = None
+        self.name = name
+        self.worker_id = worker_id
+        self.brain_name = None
+        self.brain = None
+        self.agents = None
+        self.action_size = None
+        self.state_size = None
+        self.step_count = 0
+        self.rng = None
+        self.rng_seed = None
+        self.verbose = verbose
+
+        # Initialize environment status variables    
+        self.env_info = None
+        self.state = None
+        self.reward = None
+        self.done = None
+        
+        # Create environment
+        self.env = UnityEnvironment(file_name=path, worker_id=self.worker_id, seed=seed)
+
+        # Get default Unity environment "brain"
+        self.brain_name = self.env.brain_names[0]
+        self.brain = self.env.brains[self.brain_name]
+
+        # Set seed
+        self.seed(seed)
+
+        # Reset environment
+        self.reset(train)
+
+        # Debug
+        if self.verbose:
+            print('\nNumber of agents: {}'.format(len(self.agents)))
+            print('Number of actions: {}'.format(self.action_size))
+            print('States look like: {}'.format(self.state))
+            print('States have length: {}\n'.format(self.state_size))
+        
+        return
+
+    def reset(self, train=False):
+        """
+        Reset environment
+
+        Args:
+            train (bool): Use training mode
+        
+        Returns:
+            Environment initial observation (vector)
+        
+        """
+
+        # Reset environment
+        self.env_info = self.env.reset(train_mode=train)[self.brain_name]
+
+        # Set environment variables
+        self.agents = self.env_info.agents
+        self.action_size = self.brain.vector_action_space_size
+        
+        # Set state
+        if len(self.agents) > 1:
+            self.state = self.env_info.vector_observations 
+            self.state_size = self.state.shape[1]
+        else:
+            self.state = self.env_info.vector_observations[0]
+            self.state_size = len(self.state)
+        
+        return self.state
+
+    def step(self, action=None):
+        """
+        Perform specified action in environment
+
+        Args:
+            action (int/float or List of int/float): Action to be performed
+
+        Returns:
+            Tuple containing (state, action, reward, next_state, done) 
+            
+            state (vector): Environment observation before action
+            action (int): Action to be performed
+            reward (float): Reward for performing specified action
+            state (vector): Environment observation after action
+            done (bool): Is simulation complete
+        
+        """
+        
+        # Get current state
+        state = self.state
+
+        # Send action to environment
+        self.env_info = self.env.step(action)[self.brain_name]  
+
+        # Get environment status
+        if len(self.agents) > 1:
+            next_state = self.env_info.vector_observations
+            self.reward = self.env_info.rewards
+            self.done = self.env_info.local_done
+        else:
+            next_state = self.env_info.vector_observations[0]
+            self.reward = self.env_info.rewards[0]
+            self.done = self.env_info.local_done[0]
+
+        # Set current state
+        self.state = next_state
+
+        # Increase step counter
+        self.step_count += 1
+
+        return state, action, self.reward, next_state, self.done
+
+    def close(self):
+        """
+        Close environment
+
+        Args:
+            None
+
+        Returns:
+            None
+        
+        """
+        self.env.close()
+        return
+
+    def seed(self, seed=None):
+        """
+        Set seed for random number generation, sampling, & repeatibility
+        
+        Args:
+            seed (int): Seed for random number generation
+
+        Returns:
+            None
+        
+        """
+
+        # Error check
+        if not isinstance(seed, int) and seed is not None:
+            raise ValueError('Specified seed must be integer or None')
+
+        # Set seed & random number generator
+        self.rng_seed = seed
+        self.rng = np.random.RandomState(seed)
+
+        return
+
